13039002ca57e978c6283a81be2a5cced01995d4
imirjar <azadorov1234@gmail.com>
2024-01-15 15:09:04 +0300

    post client close 1

--- internal/app/agent/client.go
+++ internal/app/agent/client.go
@@ -8,9 +8,9 @@ import (
 func sendMetric(metricType string, metric string, appURL string, value any) {
 	path := fmt.Sprintf(appURL+"/update/%s/%s/%v", metricType, metric, value)
 	resp, err := http.Post(path, "text/plain", nil)
-	defer resp.Body.Close()
+
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	defer resp.Body.Close()
 }
