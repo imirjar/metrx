1fa8d2dc7fcd181b96b4aee97c6bdfe72b2876b4
imirjar <azadorov1234@gmail.com>
2024-03-01 01:54:14 +0300

    fix

--- internal/app/server/http/handlers.go
+++ internal/app/server/http/handlers.go
@@ -49,74 +49,69 @@ func (h *HTTPGateway) UpdatePathHandler(resp http.ResponseWriter, req *http.Requ
 }
 
 func (h *HTTPGateway) UpdateJSONHandler(resp http.ResponseWriter, req *http.Request) {
-
+	// Получаем контекст запроса
 	ctx := req.Context()
+
+	// Создаем переменную для хранения метрики
 	var metric models.Metrics
 
+	// Декодируем JSON из тела запроса в переменную metric
 	err := json.NewDecoder(req.Body).Decode(&metric)
 	if err != nil {
+		// Если произошла ошибка при декодировании, отправляем ошибку "Bad Request"
 		http.Error(resp, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// Проверяем, что поля MType и ID не пустые
 	if metric.MType == "" || metric.ID == "" {
 		http.Error(resp, "MType and ID must not be empty", http.StatusBadRequest)
 		return
 	}
 
+	// Если тип метрики - "gauge", проверяем, что значение не пустое
 	if metric.MType == "gauge" && metric.Value == nil {
 		http.Error(resp, "Value must not be empty for gauge metric", http.StatusBadRequest)
 		return
 	} else if metric.MType == "counter" && metric.Delta == nil {
+		// Если тип метрики - "counter", проверяем, что дельта не пустая
 		http.Error(resp, "Delta must not be empty for counter metric", http.StatusBadRequest)
 		return
 	}
 
+	// Закрываем тело запроса после использования
 	defer req.Body.Close()
 
+	// Вызываем метод Update сервиса h с передачей контекста и метрики metric
 	newMetric, err := h.Service.Update(ctx, metric)
 	if err != nil {
+		// Если произошла ошибка при обновлении, отправляем ошибку "Bad Request"
 		http.Error(resp, err.Error(), http.StatusBadRequest)
 		return
 	}
 
+	// В зависимости от типа новой метрики, формируем соответствующий JSON-ответ
 	if newMetric.MType == "gauge" {
-		// if newMetric.Value != nil {
-		// 	log.Println("###UPDATE mValue--->", *newMetric.Value)
-		// }
+		// Если тип метрики - "gauge", преобразуем newMetric в JSON
 		jsonResponse, err := json.Marshal(newMetric)
 		if err != nil {
+			// Если произошла ошибка при маршалинге JSON, отправляем ошибку "Internal Server Error"
 			http.Error(resp, "Error marshaling JSON", http.StatusInternalServerError)
 			return
 		}
-
+		// Устанавливаем заголовок Content-Type и отправляем JSON-ответ
 		resp.Header().Set("Content-Type", "application/json")
 		resp.Write(jsonResponse)
 	} else if newMetric.MType == "counter" {
+		// Если тип метрики - "counter", устанавливаем заголовок Content-Type
 		resp.Header().Set("content-type", "application/json")
 		resp.WriteHeader(http.StatusOK)
-
+		// Отправляем JSON-ответ с помощью json.NewEncoder
 		if err = json.NewEncoder(resp).Encode(newMetric); err != nil {
 			http.Error(resp, err.Error(), http.StatusInternalServerError)
 			return
 		}
 	}
-
-	// resp.Header().Set("content-type", "application/json")
-	// resp.WriteHeader(http.StatusOK)
-
-	// if err = json.NewEncoder(resp).Encode(newMetric); err != nil {
-	// 	http.Error(resp, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-	// jsonResponse, err := json.Marshal(newMetric)
-	// if err != nil {
-	// 	http.Error(resp, "Error marshaling JSON", http.StatusInternalServerError)
-	// 	return
-	// }
-
-	// resp.Header().Set("Content-Type", "application/json")
-	// resp.Write(jsonResponse)
 }
 
 // VALUE ...
